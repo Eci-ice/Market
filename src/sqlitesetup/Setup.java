5291c8cac1938c2008b5d289008acf0fe0ef3308
Yeyuqing-Banyan <747357454@qq.com>
2023-11-17 20:54:41 +0800

    src/sqlitesetup/Setup.java

--- src/sqlitesetup/Setup.java
+++ src/sqlitesetup/Setup.java
@@ -23,68 +23,81 @@ public class Setup{
       statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
       statement.executeUpdate("DROP TABLE IF EXISTS `MLgood`");
-      statement.executeUpdate("CREATE TABLE `MLgood`  (\r\n" + 
-      		"  `goodid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" + 
-      		"  `goodname` char(20) NOT NULL,\r\n" + 
-      		"  `description` varchar(100) NOT NULL,\r\n" + 
-      		"  `price` double NOT NULL,\r\n" + 
-      		"  `picture` char(100) NOT NULL,\r\n" + 
-      		"  `state` int NOT NULL,\r\n" + 
-      		"  `number` int NOT NULL,\r\n" + 
+      statement.executeUpdate("CREATE TABLE `MLgood`  (\r\n" +
+      		"  `goodid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+      		"  `goodname` char(20) NOT NULL,\r\n" +
+      		"  `description` varchar(100) NOT NULL,\r\n" +
+      		"  `price` double NOT NULL,\r\n" +
+      		"  `picture` char(100) NOT NULL,\r\n" +
+      		"  `state` int NOT NULL,\r\n" +
+      		"  `number` int NOT NULL,\r\n" +
       		"  `kind` char(20))"
       		);
       statement.executeUpdate("DROP TABLE IF EXISTS `MLhistorygood`");
-      statement.executeUpdate("CREATE TABLE `MLhistorygood`  (\r\n" + 
-        		"  `goodid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" + 
-        		"  `goodname` char(20) NOT NULL,\r\n" + 
-        		"  `description` varchar(100) NOT NULL,\r\n" + 
-        		"  `price` double NOT NULL,\r\n" + 
-        		"  `picture` char(100) NOT NULL,\r\n" + 
-        		"  `number` int NOT NULL,\r\n" + 
+      statement.executeUpdate("CREATE TABLE `MLhistorygood`  (\r\n" +
+        		"  `goodid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+        		"  `goodname` char(20) NOT NULL,\r\n" +
+        		"  `description` varchar(100) NOT NULL,\r\n" +
+        		"  `price` double NOT NULL,\r\n" +
+        		"  `picture` char(100) NOT NULL,\r\n" +
+        		"  `state` int NOT NULL,\r\n" +
+        		"  `number` int NOT NULL,\r\n" +
         		"  `kind` char(20))"
         		);
-      
+
       statement.executeUpdate("DROP TABLE IF EXISTS `MLorder`");
-      statement.executeUpdate("CREATE TABLE `MLorder`  (\r\n" + 
-        		"  `orderid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" + 
-        		"  `address` varchar(99) ,\r\n" + 
-        		"  `telephone` varchar(11) ,\r\n" + 
-        		"  `buyername` varchar(10) ,\r\n" + 
-	        	"  `goodid` int NOT NULL,\r\n" + 
+      statement.executeUpdate("CREATE TABLE `MLorder`  (\r\n" +
+        		"  `orderid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+        		"  `address` varchar(99) ,\r\n" +
+        		"  `telephone` varchar(20) ,\r\n" +
+        		"  `buyername` varchar(99) ,\r\n" +
+	        	"  `goodid` int NOT NULL,\r\n" +
 	        	"  `orderstate` int NOT NULL ,\r\n" +
 	        	"  FOREIGN KEY (`goodid`) REFERENCES `MLgood` (`goodid`))");
 
       statement.executeUpdate("DROP TABLE IF EXISTS `MLuser`");
-      statement.executeUpdate("CREATE TABLE `MLuser`  (\r\n" + 
-      		"  `userid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" + 
-      		"  `username` char(10) NOT NULL,\r\n" + 
-      		"  `pwd` char(15) NOT NULL,\r\n" + 
-      		"  `power` int NOT NULL ,\r\n" +  
-            "  `question` varchar(50) NOT NULL,\r\n" + 
-            "  `answer` varchar(30) NOT NULL )" 
+      statement.executeUpdate("CREATE TABLE `MLuser`  (\r\n" +
+      		"  `userid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+      		"  `username` char(10) NOT NULL,\r\n" +
+      		"  `pwd` char(15) NOT NULL,\r\n" +
+      		"  `power` int NOT NULL )"
         		) ;
-      
+
       statement.executeUpdate("DROP TABLE IF EXISTS `MLprice`");
-      statement.executeUpdate("CREATE TABLE `MLprice`  (\r\n" + 
-        "  `id` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" + 
-        "  `goodid` int NOT NULL,\r\n" + 
-        "  `price` double NOT NULL,\r\n" + 
-        "  `change_time` datetime NOT NULL,\r\n" + 
+      statement.executeUpdate("CREATE TABLE `MLprice`  (\r\n" +
+        "  `id` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+        "  `goodid` int NOT NULL,\r\n" +
+        "  `price` double NOT NULL,\r\n" +
+        "  `change_time` datetime NOT NULL,\r\n" +
         "  FOREIGN KEY (`goodid`) REFERENCES `MLgood` (`goodid`))");
-      
-      
-      statement.executeUpdate("DROP TABLE IF EXISTS `MLinfo`");
-      statement.executeUpdate("CREATE TABLE `MLinfo`  (\r\n" + 
-        "  `userid` INTEGER NOT NULL,\r\n" + 
-        "  `phone` char(11) NOT NULL,\r\n" + 
-        "  `address` varchar(99) NOT NULL,\r\n" + 
+
+        //原：无 商品二级管理的表
+        statement.executeUpdate("DROP TABLE IF EXISTS `MLKindygood`");
+        statement.executeUpdate("CREATE TABLE `MLKindgood`  (\r\n" +
+                "  `goodid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+                "  `goodkindone` char(20) NOT NULL,\r\n" +
+                "  `goodkindtwo` char(20) NOT NULL,\r\n" +
+                "  FOREIGN KEY (`goodid`) REFERENCES `MLgood` (`goodid`))");
+
+
+        statement.executeUpdate("DROP TABLE IF EXISTS `MLquestion`");
+      statement.executeUpdate("CREATE TABLE `MLquestion`  (\r\n" +
+        "  `userid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+        "  `question` char(50) NOT NULL,\r\n" +
+        "  `answer` char(30) NOT NULL,\r\n" +
         "  FOREIGN KEY (`userid`) REFERENCES `MLuser` (`userid`))");
-      
-      statement.executeUpdate("INSERT INTO `MLuser` VALUES (1, '123', '123', 1,'who are you?','Xiaoming')");
-      statement.executeUpdate("INSERT INTO `MLuser` VALUES (2, '111', '111', 0,'who are you?','Xiaoming')");
-      statement.executeUpdate("INSERT INTO `MLinfo` VALUES (2, '11111111111','zjsu')");
+
+      statement.executeUpdate("DROP TABLE IF EXISTS `MLinfo`");
+      statement.executeUpdate("CREATE TABLE `MLinfo`  (\r\n" +
+        "  `userid` INTEGER PRIMARY KEY AUTOINCREMENT,\r\n" +
+        "  `phone` char(11) NOT NULL,\r\n" +
+        "  `address` char(30) NOT NULL,\r\n" +
+        "  FOREIGN KEY (`userid`) REFERENCES `MLuser` (`userid`))");
+
+      statement.executeUpdate("INSERT INTO `MLuser` VALUES (1, '123', '123', 1)");
+      statement.executeUpdate("INSERT INTO `MLuser` VALUES (2, '111', '111', 0)");
       System.out.println("Success Setup");
-      
+
 
     }
     catch(SQLException e){
